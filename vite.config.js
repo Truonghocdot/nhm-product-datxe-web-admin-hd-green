0099f03d1cb208dd9cec987c1888044d8dfead09
Truonghocdot <truonghocdot05@gmail.com>
2026-06-12 17:59:20 +0700

    implement

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'serve' ? '/' : '/admin/',
   server: {
     port: 5173,
     strictPort: true,
